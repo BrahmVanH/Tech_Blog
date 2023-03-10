c8d57ecbee0aab54b84d56b00e73999e06afb4dd
Brahm V <brahmvanh@gmail.com>
2023-03-10 17:54:23 -0500

    Started blog-routes. Created homepageRoutes for small selection of blog posts to display.

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,9 +1,57 @@
 const router = require("express").Router();
-const { Blog } = require("../../models");
+const { Blog, User, Comment } = require("../../models");
 
 router.get("/", async(req, res) => {
     try {
-        const blogData = await Blog.findAll
-    }
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['content', 'user_id'],
+                },
+            ],
+        });
+
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    
+        res.render('all-blogs', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try{ 
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['content', 'user_id'],
+                }
+            ],
+        });
+
+        const blog = blogData.get({ plain: true});
+
+        res.render('blog-search', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+    
+
